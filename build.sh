a4a921d90dde24ffe70e6c72585d9d6dc629c6cf
djluo <dj.luo@baoyugame.com>
2015-06-24 17:55:56 +0800

    add zh_CN

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# vim:set et ts=2 sw=2:
+
+# Author : djluo
+
+current_dir=`dirname $0`
+current_dir=`readlink -f $current_dir`
+cd ${current_dir} && export current_dir
+
+registry="docker.xlands-inc.com"
+repo=$(dirname   ${current_dir})
+#repo=$(dirname   ${repo})
+repo=$(basename  ${repo})
+repo=${repo/-/_}
+image=$(basename ${current_dir})
+full_image="${registry}/${repo}/${image}"
+ver="${1-latest}"
+
+usage() {
+  echo "Usage: $0         # building and tag is latest"
+  echo "Usage: $0 [x.y.z] # building and tag is x.y.z"
+  echo
+  exit 127
+}
+
+if [ "x$1" == "x-h" -o "x$1" == "x--help" ];then
+  usage
+fi
+
+cid=0
+sudo docker build -t ${full_image}:${ver} . \
+  && cid=$(sudo docker images -q ${full_image} | head -1)
+
+#backup="${current_dir}/images/${image}_${ver}_${cid}.gz"
+#
+#if [ "x$cid" != "x0" ] && [ ! -f $backup ];then
+#  sudo docker save ${cid} | gzip > ./images/${image}_${ver}_${cid}.gz
+#else
+#  echo "$backup exist"
+#fi
